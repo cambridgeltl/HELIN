d1d1d2a01fcbcca3deb7497cdc17d1e8d1190d24
Zaiqiao Meng <zaiqiao.meng@gmail.com>
2021-03-02 14:36:27 +0000

    add route for entity linking only

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+
 from tagger import Tagger
 
 app = Flask(__name__)
@@ -10,6 +11,12 @@ def tag_string():
     original_text = request.args.get('txt', '', type=str)
     text, entities = tagger.tag(original_text)
     return jsonify(text=text, entities=entities)
+
+@app.route('/link_entity')
+def link_entity():
+    original_text = request.args.get('txt', '', type=str)
+    snomed_name, snomed_id = tagger.normalize(original_text)
+    return jsonify(text=original_text, entities=[f"{snomed_name} ({snomed_id})"])
 
 
 @app.route('/')
